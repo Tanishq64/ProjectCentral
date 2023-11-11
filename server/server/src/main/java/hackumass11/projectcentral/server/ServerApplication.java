bed2806c0d7c735bba4c25ed970ca899db832db6
omzz15 <ompatel1359@gmail.com>
2023-11-11 00:50:38 -0500

    added server

---
+++ server/server/src/main/java/hackumass11/projectcentral/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package hackumass11.projectcentral.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
